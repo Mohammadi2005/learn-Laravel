2ab00ee86eda65eaceb8a783088d9a8b8ee8209b
amir hossein <amohammadi161183@gmail.com>
2025-08-09 09:45:49 +0330

    connect to database with model

--- Laravel/app/Http/Controllers/TemplateController.php
+++ Laravel/app/Http/Controllers/TemplateController.php
@@ -2,8 +2,30 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 
 class TemplateController extends Controller
 {
+    public function goHome(){
+
+        $users = User::all();
+        $users->map(function($user){
+            dump($user->email);
+        });
+
+        $users = User::where('id','>', 1)
+            ->where('id', '<', 3)
+            ->tosql();
+        dump($users);
+
+        $users = User::where('id','<', 2)
+            ->orwhere('id', '>', 2)
+            ->get();
+        foreach($users as $user){
+            dump($user->email);
+        }
+
+        return view('home');
+    }
 }
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TemplateController;
 
 // send view
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [
+    TemplateController::class,
+    "goHome"
+]);
 
 Route::get('/posts', function () {
     return view('post');
